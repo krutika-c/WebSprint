169bcfef14bdf530d744c03f45cac85cc2bda4eb
Vindhya <vindhyaasmitha@gmail.com>
2026-07-27 22:11:41 +0530

    Refernced all the pages of the project

--- backend/src/main/java/com/websprint/backend/Controller/ContentController.java
+++ backend/src/main/java/com/websprint/backend/Controller/ContentController.java
@@ -20,4 +20,44 @@ public class ContentController {
     public String home() {
         return "h1";
     }
+
+    @GetMapping("/choose-topic") 
+    public String chooseTopic() {
+        return "choose-topic";
+    }
+
+    @GetMapping("/html-roadmap") 
+    public String htmlMap() {
+        return "html-roadmap";
+    }
+
+    @GetMapping("/css-roadmap") 
+    public String cssMap() {
+        return "css-roadmap";
+    }
+
+    @GetMapping("/js-roadmap") 
+    public String jsMap() {
+        return "js-roadmap";
+    }
+
+    @GetMapping("/dashboard") 
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping("/achivements") 
+    public String achivements() {
+        return "achivements";
+    }
+
+    @GetMapping("/settings") 
+    public String settings() {
+        return "settings";
+    }
+
+    @GetMapping("/profile") 
+    public String profile() {
+        return "profile";
+    }
 }
