28b3ce03f7303cf4977d8d4425295d82ee0f9ca2
Vindhya <vindhyaasmitha@gmail.com>
2026-08-22 22:41:15 +0530

    Changed the port number from 5500 to 5501 in cors config

--- backend/src/main/java/com/websprint/backend/Config/CorsConfig.java
+++ backend/src/main/java/com/websprint/backend/Config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://127.0.0.1:5500");
+                        .allowedOrigins("http://127.0.0.1:5501");
             }
         };
     }
